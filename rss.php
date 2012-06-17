5c7b96e16e0aaf771aa31280ab977dbde5d0eec7
tale <547475491@qq.com>
2012-06-18 07:08:48 +0800

    伪造一些文件，让文件框架完善...

--- rss.php
+++ rss.php
@@ -2,11 +2,12 @@
 $username = 'taletao'; 
 $feedURL = 'http://www.cnblogs.com/taletao/rss';
 $excludePattern = '/' . $username . ': @/';
-$count = 5; //  show count
+$count = 5;
 $i = 0;
 
 if (!$xml = simplexml_load_file($feedURL)) {
     trigger_error('Error', E_USER_ERROR);
 }
 
+include './tpl/blog.html';
 ?>
